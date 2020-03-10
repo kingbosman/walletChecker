d56ce25121ff336c79130b0fc4f5bc20e56124e3
Robin Bosman <r.bosman@office.litebit.nl>
2020-03-10 15:54:11 +0100

    added todo for next touch

--- api/controllers/Bitcoin.js
+++ api/controllers/Bitcoin.js
@@ -1,1 +1,1 @@
-// Function to update wallet detail per address , keep address and add last updated throw wrong address
+//TODO Function to update wallet detail per address , keep address and add last updated throw wrong address
